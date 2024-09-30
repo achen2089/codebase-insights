124d06f9c774ac26a978f540d952931e02ba1b9b
Achen2089 <anthonychen2089@gmail.com>
2024-09-29 21:06:15 -0700

    add session provider to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import "./globals.css"
+import { SessionProvider } from "next-auth/react"
+import { auth } from "@/auth"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import Header from "@/components/header"
@@ -11,16 +13,19 @@ export const metadata: Metadata = {
     "Get insights into your codebase",
 }
 
-export default function RootLayout({ children }: React.PropsWithChildren) {
+export default async function RootLayout({ children }: React.PropsWithChildren) {
+  const session = await auth()
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="flex h-full min-h-screen w-full flex-col justify-between">
-          <Header />
-          <main className="mx-auto w-full max-w-3xl flex-auto px-4 py-4 sm:px-6 md:py-6">
-            {children}
-          </main>
-        </div>
+        <SessionProvider session={session}>
+          <div className="flex h-full min-h-screen w-full flex-col justify-between">
+            <Header />
+            <main className="mx-auto w-full max-w-3xl flex-auto px-4 py-4 sm:px-6 md:py-6">
+              {children}
+            </main>
+          </div>
+        </SessionProvider>
       </body>
     </html>
   )
